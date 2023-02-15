5076350e2b8420adf2812831dae767dde6bd8c7e
Benjamin Leveque <104634127+BenBishop40@users.noreply.github.com>
2023-02-15 18:12:56 +0100

    ajou about me debug texte anim JS Hello World

--- src/components/about-me/AboutMe.jsx
+++ src/components/about-me/AboutMe.jsx
@@ -1,21 +1,80 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './_about_me.scss';
+// import pictCartoon from '../../img/pic_id_cartoon.jpg';
 
-class AboutMe extends Component {
-    render() {
-        return (
-            <div className="about-me">
-                <img src="" alt="" className="about-me-pic" />
-                <h1>This pen is
-                <span
-                    class="txt-rotate"
-                    data-period="2000"
-                    data-rotate='[ "nerdy.", "simple.", "pure JS.", "pretty.", "fun!" ]'></span>
-                </h1>
-                <h2>A single &lt;span&gt; is all you need.</h2>
-            </div>
-        )
+
+class TxtRotate extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        toRotate: JSON.parse(props.toRotate),
+        loopNum: 0,
+        txt: '',
+        isDeleting: false,
+      };
     }
-}
-
-export default AboutMe
+  
+    componentDidMount() {
+      this.tick();
+    }
+  
+    tick() {
+      const { toRotate, loopNum, txt, isDeleting } = this.state;
+      const i = loopNum % toRotate.length;
+      const fullTxt = toRotate[i];
+  
+      if (isDeleting) {
+        this.setState({ txt: fullTxt.substring(0, txt.length - 1) });
+      } else {
+        this.setState({ txt: fullTxt.substring(0, txt.length + 1) });
+      }
+  
+      let delta = 300 - Math.random() * 100;
+      const that = this;
+  
+      if (isDeleting) {
+        delta /= 2;
+      }
+  
+      if (!isDeleting && txt === fullTxt) {
+        this.setState({ isDeleting: true });
+        delta = this.props.period;
+      } else if (isDeleting && txt === '') {
+        this.setState(prevState => ({
+          isDeleting: false,
+          loopNum: prevState.loopNum + 1,
+          txt: '',
+        }));
+        delta = 1000;
+      }
+  
+      setTimeout(() => {
+        that.tick();
+      }, delta);
+    }
+  
+    render() {
+      return (
+        <span className="txt-rotate">
+          <span className="wrap">{this.state.txt}</span>
+        </span>
+      );
+    }
+  }
+  
+  export default class AboutMe extends Component {
+    render() {
+      return (
+        <div className='about-me'>
+          <p>
+            Hello,{' '}
+            <TxtRotate
+              toRotate='["World !", "Visiteur !", "Recruteur ?!"]'
+              period="5000"
+            />
+          </p>
+        </div>
+      );
+    }
+  }
